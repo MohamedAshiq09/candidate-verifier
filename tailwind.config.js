f3276b149097f42c70b82cd8c332c70a6e495656
MohamedAshiq09 <mohamedashiq782@gmail.com>
2025-07-01 13:03:43 +0530

    Refactor Tailwind CSS configuration and update TypeScript settings

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     darkMode: ["class"],
-    content: [],
+    content: [
+      './pages/**/*.{js,ts,jsx,tsx,mdx}',
+      './components/**/*.{js,ts,jsx,tsx,mdx}',
+      './app/**/*.{js,ts,jsx,tsx,mdx}',
+      './src/**/*.{js,ts,jsx,tsx,mdx}',
+    ],
   theme: {
   	extend: {
   		borderRadius: {
@@ -55,4 +60,3 @@ module.exports = {
   },
   plugins: [require("tailwindcss-animate")],
 }
-
